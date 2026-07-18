18ce3afe97f9fd54f5c3573ba85c28d39cffbd5c
Vynts <vendzaky@gmail.com>
2026-07-18 11:51:29 +0700

    feat: add sitemap

---
+++ sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = 'https://erza.site'; 
+
+  const routes = ['', '/about', '/posts', '/repositories'].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+    changeFrequency: 'weekly' as const,
+    priority: route === '' ? 1.0 : 0.8,
+  }));
+
+  return [...routes];
+}
